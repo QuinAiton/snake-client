a5be66373bb06cb01012ed9e6373ef74fa5c099d
QuintenAiton <quinaiton@hotmail.com>
2021-01-13 22:10:08 +0000

    connected the conn command tothe input file and wrote move commands to the handler function handleUserInput

--- input.js
+++ input.js
@@ -1,11 +1,25 @@
+//stores the active TCP connection object
+let connection;
+
+
 const handleUserInput = (char) => {
   if (char === '\u0003' || char === 'x') {
     process.exit();
+  } else if (char === 'w') {
+    connection.write('Move: up');
+  } else if (char === 'd') {
+    connection.write('Move: right');
+  } else if (char == 's') {
+    connection.write('Move: down');
+  } else if (char === 'a') {
+    connection.write('Move: left');
+
   }
 }
 
 //setup User Interface to handle user input via stdin
-const setUpInput = () => {
+const setUpInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
