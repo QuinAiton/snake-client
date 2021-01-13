60a57db3cc7ef198a88e7951529497bd00ee8f4b
QuintenAiton <quinaiton@hotmail.com>
2021-01-13 20:18:21 +0000

    wote code to connect to the server and take in incomming data from server

---
+++ play.js
@@ -0,0 +1,20 @@
+const net = require('net');
+
+//establish connection with the game server 
+const connect = () => {
+  const conn = net.createConnection({
+    host: '10.0.2.15',
+    port: 50541
+
+  })
+
+  conn.on('data', (data) => {
+    console.log(`server: ${data}`)
+  });
+  //interpret incoming data as text
+  conn.setEncoding('utf8');
+
+  return conn;
+}
+console.log('connecting....');
+connect();
