f0ca0bfa76e0527e567113003946796d65b16ec9
QuintenAiton <quinaiton@hotmail.com>
2021-01-13 21:33:33 +0000

    created setUpInput function to take in input from the user and a handleUserInput function that exits program

--- play.js
+++ play.js
@@ -1,3 +1,21 @@
 const connect = require('./client');
 console.log('connecting....');
 connect();
+
+const handleUserInput = (char) => {
+  if (char === '\u0003' || char === 'x') {
+    process.exit();
+  }
+}
+
+//setup User Interface to handle user input via stdin
+const setUpInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+}
+
+setUpInput();
